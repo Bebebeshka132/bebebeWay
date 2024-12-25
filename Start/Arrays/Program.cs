4a05cb036d483823f8081fbbd696e5283167abfa
Bebebeshka <poroshok397@gmail.com>
2024-12-25 22:39:31 +0800

    bebebe

---
+++ Start/Arrays/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] mas = { { { 1, 2 },{ 3, 4 } },
+                { { 4, 5 }, { 6, 7 } },
+                { { 7, 8 }, { 9, 10 } },
+                { { 10, 11 }, { 12, 13 } }
+              };
+
+            int x = mas.GetUpperBound(0);
+            int y = mas.GetUpperBound(1);
+            int z = mas.GetUpperBound(2);
+
+            Console.Write("{");
+
+            for (int i = 0; i <= x; i++)
+            {
+                    Console.Write("{");
+                    for (int a = 0; a <= y; a++)
+                    {
+                    Console.Write("{");
+
+                            for (int b = 0; b <= z; b++)
+                            {
+                                Console.Write(mas[i, a, b]);
+                                if (b < z)
+                                {
+                                    Console.Write(",");
+                                }
+                            }
+
+                    Console.Write("}");
+                    if (a < y)
+                        {
+                            Console.Write(",");
+                        }
+                    }
+
+                Console.Write("}");
+                if (i < x)
+                {
+                    Console.Write(",");
+                }
+            }
+
+            Console.Write("}");
+
+
+        }
+    }
+}
--- Start/ConsoleApp1/Program.cs
+++ Start/ConsoleApp1/Program.cs
@@ -10,8 +10,63 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //decimal sum, interest;
+            //int month, i;
+
+            //i = 0;
+
+            //Console.WriteLine("Калькулятор вкладов");
+
+            //sum = decimal.Parse(Console.ReadLine());
+            //interest = decimal.Parse(Console.ReadLine());
+            //month = int.Parse(Console.ReadLine());
+
+            //interest = interest / 100;
+
+            //while (i < month)
+            //{
+            //    i++;
+            //    sum += sum * interest;
+            //    Console.WriteLine(sum);
+            //}
+
+            //Console.WriteLine(sum);
+
+
+
+
+
+            //Console.WriteLine("Таблица умножения");
+
+            //for(int i = 1; i <= 10; i++)
+            //{
+            //    //Console.WriteLine("==================================================================================================================");
+            //    for(int a = 1; a <= 10; a++)
+            //    {
+            //        //Console.WriteLine(a * i);
+            //        Console.Write($"{i * a}\t");
+            //    }
+            //}
 
             
+            //while (true)
+            //{
+            //    Console.WriteLine("Введите два числа от 0 до 10");
+
+            //    int a = int.Parse(Console.ReadLine());
+            //    int b = int.Parse(Console.ReadLine());
+
+            //    if ((a >= 0 && a <= 10) && (b >= 0 && b <= 10))
+            //    {
+            //        Console.WriteLine(a * b);
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Упс, вы ввели не тот диапазон чисел!");
+            //        continue;
+            //    }
+            //}
         }
     }
 }
