f19bcb642137a8fae3c3c3dcf48cc40db354c0bb
Bebebeshka <poroshok397@gmail.com>
2025-01-05 20:23:13 +0800

    New Calc with methods

--- Start/Methods/Program.cs
+++ Start/Methods/Program.cs
@@ -10,6 +10,62 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            void BebebeText()
+            {
+                Console.WriteLine("Hello, Bebebe");
+            }
+
+            BebebeText();
+
+            //=================================================================
+
+            double c = 0;
+            string operand;
+
+            Console.WriteLine("CALCULATOR");
+
+            Console.WriteLine("Первое число");
+            double a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Действие");
+            operand = Console.ReadLine();
+
+            Console.WriteLine("Второе число");
+            double b = int.Parse(Console.ReadLine());
+
+            void Calc(double f, double k, ref double be, string op)
+            {
+                
+                switch (op)
+                {
+                    case "+":
+                        {
+                            be = f + k;
+                        }
+                        break;
+
+                    case "-":
+                        {
+                            be = f + k;
+                        }
+                        break;
+
+                    case "*":
+                        {
+                            be = f * k;
+                        }
+                        break;
+
+                    case "/":
+                        {
+                            be = f / k;
+                        }
+                        break;
+                }
+            }
+
+            Calc(a, b, ref c, operand);
+            Console.WriteLine(c);
         }
     }
 }
