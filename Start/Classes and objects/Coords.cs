4654ef72f4188b3343a9e2a498a96a4c0f45f6b9
Bebebeshka <poroshok397@gmail.com>
2025-01-06 20:30:40 +0800

    конец сессии

---
+++ Start/Classes and objects/Coords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_objects
+{
+    internal class Coords
+    {
+        public int firstBodyX;
+        public int firstBodyY;
+        public int secondBodyX;
+        public int secondBodyY;
+
+
+        public double Distance()
+        {
+            int a = firstBodyX - secondBodyX;
+            int b = firstBodyY - secondBodyY;
+            double c;
+
+            return c = Math.Sqrt((a * a) + (b * b));
+        }
+
+        public Coords (int firstBodyX, int firstBodyY, int secondBodyX, int secondBodyY)
+        {
+            this.firstBodyX = firstBodyX;
+            this.firstBodyY = firstBodyY;
+            this.secondBodyX = secondBodyX; 
+            this.secondBodyY = secondBodyY; 
+        }
+
+    }
+}
--- Start/Classes and objects/Program.cs
+++ Start/Classes and objects/Program.cs
@@ -6,27 +6,14 @@ using System.Threading.Tasks;
 
 namespace Classes_and_objects
 {
-    class Person
-    {
-        public string name = "Undefined";   // имя
-        public int age;                     // возраст
-
-        public void Print()
-        {
-            Console.WriteLine($"Имя: {name}  Возраст: {age}");
-        }
-    }
-
-
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person a = new Person();
-
-            string name = a.name;
-            Console.WriteLine(name);
-
+            Coords a = new Coords(5, 8, 3, 17);
+            
+            double result = a.Distance();
+            Console.WriteLine(result);
         }
     }
 }
