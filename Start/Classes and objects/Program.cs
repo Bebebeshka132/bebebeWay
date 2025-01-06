aa5adb65069da6730cecafc525733f435c1a7efd
Bebebeshka <poroshok397@gmail.com>
2025-01-06 19:06:05 +0800

    поправленная версия файлов с обновлённым dotnet

--- Start/Classes and objects/Program.cs
+++ Start/Classes and objects/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace Classes_and_objects
 {
+    class Person
+    {
+        public string name = "Undefined";   // имя
+        public int age;                     // возраст
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person a = new Person();
+
+            string name = a.name;
+            Console.WriteLine(name);
+
         }
     }
 }
+
+
+
+
+
+
+
--- Start/Methods/Properties/AssemblyInfo.cs
+++ Start/Methods/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// Общие сведения об этой сборке предоставляются следующим набором
-// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
-// связанные с этой сборкой.
-[assembly: AssemblyTitle("Methods")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Methods")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,13 +11,3 @@ using System.Runtime.InteropServices;
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
 [assembly: Guid("0b9c4b08-4421-4319-b50e-21b67f8fa3a8")]
-
-// Сведения о версии сборки состоят из указанных ниже четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//      Номер сборки
-//      Номер редакции
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
